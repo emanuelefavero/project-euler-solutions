857d154dd39ca6e7af76d6d75c007c84130a95a3
Emanuele Favero <emafaux89@gmail.com>
2024-12-06 01:53:26 +0100

    add solutions/1 jsDoc

--- solutions/1.js
+++ solutions/1.js
@@ -3,6 +3,11 @@
 // If we list all the natural numbers below $10$ that are multiples of $3$ or $5$, we get $3, 5, 6$ and $9$. The sum of these multiples is $23$.
 // Find the sum of all the multiples of $3$ or $5$ below $1000$
 
+/**
+ * @returns {number}
+ */
+
+// O(n) time | O(1) space
 function main() {
   let n = 1000
   let sum = 0
